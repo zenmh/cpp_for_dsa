b8bf60b5404f1e8e7e490f09f838fdceb175dd8d
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-20 10:59:05 +0600

    Dynamic variable

--- dynamic memory allocation/dynamic_variable.cpp
+++ dynamic memory allocation/dynamic_variable.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
+      int *a = new int;
+      *a = 10;
+
+      float *b = new float;
+      *b = 4.34;
+
+      cout << *a << endl
+           << *b << endl;
+
       return 0;
 }
