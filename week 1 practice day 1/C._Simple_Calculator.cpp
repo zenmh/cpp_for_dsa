878933c52d4d60e5571c9a4975af2f9982159e6a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-20 18:13:39 +0600

    Simple Calculator

---
+++ week 1 practice day 1/C._Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      long long a, b;
+      cin >> a >> b;
+
+      cout << a << " + " << b << " = " << a + b << endl
+           << a << " * " << b << " = " << a * b << endl
+           << a << " - " << b << " = " << a - b;
+
+      return 0;
+}
