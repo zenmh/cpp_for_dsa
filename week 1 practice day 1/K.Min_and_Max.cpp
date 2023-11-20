b337cfb29b64214f518406765ee6dda08a5dfea5
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-20 18:38:41 +0600

    Min and Max

---
+++ week 1 practice day 1/K.Min_and_Max.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int a, b, c;
+      cin >> a >> b >> c;
+
+      cout << min({a, b, c}) << " " << max({a, b, c});
+
+      return 0;
+}
