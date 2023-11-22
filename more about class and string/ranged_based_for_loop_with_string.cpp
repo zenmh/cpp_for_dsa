d3cda2010a3bafccaf090f949528474f362821b3
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-22 23:04:56 +0600

    Ranged base for loop applyed on string

---
+++ more about class and string/ranged_based_for_loop_with_string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      string s;
+      getline(cin, s);
+
+      for (char c : s)
+            cout << c << endl;
+
+      return 0;
+}
