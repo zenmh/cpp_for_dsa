74e5a59be5db585f6ec2bcf894b47c385067da26
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-22 22:15:43 +0600

    Int and string input with multiline

---
+++ string and class in cpp/int_and_string_input_with_multiline.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int x;
+      string s;
+      cin >> x;
+
+      // cin.ignore(); // or
+      getchar();
+      getline(cin, s);
+
+      cout << x << endl
+           << s;
+
+      return 0;
+}
