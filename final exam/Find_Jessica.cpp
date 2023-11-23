5a9d6ea93259b3cb2c071fbd36aef62680f03763
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-23 21:26:01 +0600

    Find Jessica

---
+++ final exam/Find_Jessica.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int f = 0;
+      string s, word;
+      getline(cin, s);
+
+      stringstream ss(s);
+
+      while (ss >> word)
+      {
+            if (word == "Jessica")
+                  f = 1;
+      }
+
+      if (f == 1)
+            cout << "YES";
+      else
+            cout << "NO";
+
+      return 0;
+}
