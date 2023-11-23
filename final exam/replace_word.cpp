c5b9dac79f841f04fa79fabdd1496ce7a3ed79d1
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-23 21:17:32 +0600

    Replace Word

--- final exam/replace_word.cpp
+++ final exam/replace_word.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
+      int tc;
+      cin >> tc;
+
+      for (int o = 0; o < tc; o++)
+      {
+            string s, x;
+            cin >> s >> x;
+
+            while (s.find(x) != -1)
+            {
+                  s.replace(s.find(x), x.size(), "#");
+            }
+
+            cout << s << endl;
+      }
 
       return 0;
 }
